d3a24b1dd8cc5893b754fa2cf517dc51d1f32c37
Thiago dos Santos <thiagosantos@Thiagos-Mac-mini.local>
2020-01-26 19:25:18 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/thisan/imc_calculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.thisan.imc_calculator
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
